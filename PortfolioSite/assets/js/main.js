fe9188a88fb003596cbc1716f9b69bc19992eb00
bemibrando <102377919+bemibrando@users.noreply.github.com>
2022-03-26 20:19:43 -0300

    Skills, almost done

--- PortfolioSite/assets/js/main.js
+++ PortfolioSite/assets/js/main.js
@@ -21,12 +21,28 @@ const navLink = document.querySelectorAll('.nav__link')
 
 function linkAction(){
     const navMenu = document.getElementById('nav-menu')
-    navMenu.classList.remove('show-menu')
+    navMenu.classList.remove('show-menu') 
 }
 navLink.forEach(n => n.addEventListener('click', linkAction))
 
 /*=========== ACCORDION SKILLS ===========*/
+const skillContent = document.getElementsByClassName('skills__content'),
+    skillsHeader = document.querySelectorAll('.skills__header')
 
+function toggleSkills(){
+    let itemClass = this.parentNode.className
+
+    for(i = 0; i < skillContent.length; i++){
+        skillContent[i].className = 'skills__content skills__close'
+    }
+    if(itemClass === 'skills__content skills__close'){
+        this.parentNode.className = 'skills__content skills__open'
+    }
+}
+
+skillsHeader.forEach((el) =>{
+    el.addEventListener('click', toggleSkills)
+})
 
 /*=========== QUALIFICATIONS TABS ===========*/
 
