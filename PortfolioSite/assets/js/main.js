0c554135170b86de0b6f345e317dd915336dde66
Bianca Emi <102377919+bemibrando@users.noreply.github.com>
2022-03-28 18:10:00 -0300

    timeline & services add

--- PortfolioSite/assets/js/main.js
+++ PortfolioSite/assets/js/main.js
@@ -45,7 +45,52 @@ skillsHeader.forEach((el) =>{
 })
 
 /*=========== QUALIFICATIONS TABS ===========*/
+const tabs = document.querySelectorAll('[data-target]'),
+    tabContents = document.querySelectorAll('[data-content]')
 
+tabs.forEach(tab => {
+    tab.addEventListener('click', () =>{
+        const target = document.querySelector(tab.dataset.target)
+
+        tabContents.forEach(tabContent =>{
+            tabContent.classList.remove('qualification__active')
+        })
+        target.classList.add('qualification__active')
+
+        tab.forEach(tab =>{
+            tab.classList.remove('qualification__active')
+        })
+        tab.classList.add('qualification__active')
+    })
+})
 
 /*=========== SERVICES MODAL ===========*/
+const modalViews = document.querySelectorAll('.services__modal'),
+    modalBtns = document.querySelectorAll('.services__button'),
+    modalCloses = document.querySelectorAll('.services__modal-close')
+
+let modal = function(modalClick){
+    modalViews[modalClick].classList.add('active-modal')
+}
+
+modalBtns.forEach((modalBtn, i) => {
+    modalBtn.addEventListener('click', () =>{
+        modal(i)
+    })
+})
+
+modalCloses.forEach((modalClose) =>{
+    modalClose.addEventListener('click', () =>{
+        modalViews.forEach((modalViews) =>{
+            modalViews.classList.remove('active-modal')
+        })
+    })
+})
+
+/*=========== PORTFOLIO SWIPER ===========*/
+/*=========== TESTIMONIAL ===========*/
+/*=========== SCROLL SECTIONS ACTIVE LINK ===========*/
+/*=========== CHANGE BACKGROUND HEADER ===========*/
+/*=========== SHOW SCROLL TOP ===========*/
+
 
