07157e8992b7ef1562ef25929f4625288a45db25
bemibrando <bemi.brando@outlook.com>
2022-05-06 13:16:18 -0300

    HTML markup done

--- LandingPageChallenge/assets/js/main.js
+++ LandingPageChallenge/assets/js/main.js
@@ -1,5 +1,4 @@
 //#region DARK / LIGHT THEME
-/*=========== DARK / LIGHT THEME ===========*/
 const themeButton = document.getElementById('theme-button')
 const darkTheme = 'dark-theme'
 const iconTheme = 'uil-sun'
@@ -29,4 +28,53 @@ themeButton.addEventListener('click', () => {
     localStorage.setItem('selected-icon', getCurrentIcon)
 })
 
+//#endregion
+
+//#region MENU 
+const navMenu = document.getElementById('nav-menu'),
+    navToggle = document.getElementById('nav-toggle'),
+    navClose = document.getElementById('nav-close')
+
+// *===== MENU SHOW ======*/
+if(navToggle){
+    navToggle.addEventListener('click', () =>{
+        navMenu.classList.add('show-menu')
+    })
+}
+// *===== MENU HIDDEN ======*/
+if(navClose){
+    navClose.addEventListener('click', () =>{
+        navMenu.classList.remove('show-menu')
+    })
+}
+
+/*=========== REMOVE MENU MOBILE ===========*/
+const navLink = document.querySelectorAll('.nav__link')
+
+function linkAction(){
+    const navMenu = document.getElementById('nav-menu')
+    navMenu.classList.remove('show-menu') 
+}
+navLink.forEach(n => n.addEventListener('click', linkAction))
+
+//#endregion
+
+//#region ACCORDION SKILLS 
+const skillContent = document.getElementsByClassName('skills__content'),
+    skillsHeader = document.querySelectorAll('.skills__header')
+
+function toggleSkills(){
+    let itemClass = this.parentNode.className
+
+    for(i = 0; i < skillContent.length; i++){
+        skillContent[i].className = 'skills__content skills__close'
+    }
+    if(itemClass === 'skills__content skills__close'){
+        this.parentNode.className = 'skills__content skills__open'
+    }
+}
+
+skillsHeader.forEach((el) =>{
+    el.addEventListener('click', toggleSkills)
+})
 //#endregion
--- assets/js/main.js
+++ assets/js/main.js
@@ -144,7 +144,7 @@ function scrollActive(){
         const sectionTop = current.offsetTop - 50;
         sectionId = current.getAttribute('id')
 
-        if(scrollY > sectionTop && scrrollY <= sectionTop + sectionHeight){
+        if(scrollY > sectionTop && scrollY <= sectionTop + sectionHeight){
             document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.add('active-link')
         }else{
             document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.remove('active-link')
